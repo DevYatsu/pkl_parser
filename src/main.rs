efd1fc23c3a05340e0fee3e4668e4a5659fccc7f
Yatsu <yatsu.dev@gmail.com>
2024-08-19 17:20:49 +0200

    parsing working nicely

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,70 @@
-use new_pkl::{
-    pest::{parse, Rule},
-    Pkl, PklError,
-};
-use std::{env::args, fs, time::Instant};
+use pkl_parser::{parse, Rule};
+use std::{fs, time::Instant};
 
-fn main() -> Result<(), (String, String, Option<String>)> {
-    let args = args().into_iter().collect::<Vec<_>>();
+fn main() {
+    let src = fs::read_to_string("a.pkl").unwrap();
 
-    match args.get(1) {
-        Some(_) => {
-            let src = fs::read_to_string("main.pkl").unwrap();
+    let src = src.repeat(1);
+    let time = Instant::now();
 
-            let src = src.repeat(1000);
-            let time = Instant::now();
+    let mut result = parse(&src).unwrap();
 
-            let mut pkl = Pkl::new();
+    let file = result.next().unwrap().into_inner();
 
-            let ast = pkl.generate_ast(&src).map_err(|e: PklError| {
-                (
-                    e.msg().to_owned(),
-                    src[e.span().unwrap(/* safe */)].to_owned(),
-                    e.file_name().to_owned(),
-                )
-            })?;
-            // pkl.parse(&src).map_err(|e: PklError| {
-            //     (
-            //         e.msg().to_owned(),
-            //         src[e.span().unwrap(/* safe */)].to_owned(),
-            //         e.file_name().to_owned(),
-            //     )
-            // })?;
-
-            // for stmt in pkl.generate_ast(&src).unwrap() {
-            //     println!("{stmt:?}",);
-            // }
-
-            println!(
-                "{}ms to parse {} chars",
-                time.elapsed().as_millis(),
-                src.len()
-            );
-
-            println!("{:?}", pkl);
-        }
-        None => {
-            let src = fs::read_to_string("a.pkl").unwrap();
-
-            let src = src.repeat(1);
-            let time = Instant::now();
-
-            let mut result = parse(&src).unwrap();
-
-            let file = result.next().unwrap().into_inner();
-
-            let mut doc_comment: Option<String> = None;
-            for record in file {
-                match record.as_rule() {
-                    Rule::stmt => {
-                        for stmt in record.into_inner() {
-                            match stmt.as_rule() {
-                                _ => {
-                                    // when managing the statement
-                                    // take the value inside the doc
-                                    // comment to attach it to attach
-                                    // it to the stmt if necessary
-                                    for stmt in stmt.into_inner() {
-                                        match stmt.as_rule() {
-                                            Rule::COMMENT => {
-                                                // println!("comment: {:?}", stmt.as_rule())
-                                            }
-                                            _ => {
-                                                // println!("any stmt: {:?}", stmt.as_rule());
-                                            }
-                                        }
+    let mut doc_comment: Option<String> = None;
+    for record in file {
+        match record.as_rule() {
+            Rule::stmt => {
+                for stmt in record.into_inner() {
+                    match stmt.as_rule() {
+                        _ => {
+                            // when managing the statement
+                            // take the value inside the doc
+                            // comment to attach it to attach
+                            // it to the stmt if necessary
+                            for stmt in stmt.into_inner() {
+                                match stmt.as_rule() {
+                                    Rule::COMMENT => {
+                                        // println!("comment: {:?}", stmt.as_rule())
+                                    }
+                                    _ => {
+                                        // println!("any stmt: {:?}", stmt.as_rule());
                                     }
                                 }
                             }
                         }
                     }
-                    Rule::EOI => (),
-                    Rule::COMMENT => {
-                        for com in record.into_inner() {
-                            match com.as_rule() {
-                                Rule::doc_comment => {
-                                    doc_comment = Some(
-                                        doc_comment
-                                            .map(|mut x| {
-                                                x.push_str(com.as_str());
-                                                x
-                                            })
-                                            .unwrap_or(String::from(com.as_str())),
-                                    );
-                                }
-                                Rule::annotation => {
-                                    // take care of them here
-                                }
-                                Rule::line_comment | Rule::multiline_comment => (),
-                                _ => unreachable!(),
-                            }
-                        }
-                    }
-                    _ => unreachable!(),
                 }
             }
-
-            println!(
-                "{}ms to parse {} chars",
-                time.elapsed().as_millis(),
-                src.len()
-            );
+            Rule::EOI => (),
+            Rule::COMMENT => {
+                for com in record.into_inner() {
+                    match com.as_rule() {
+                        Rule::doc_comment => {
+                            doc_comment = Some(
+                                doc_comment
+                                    .map(|mut x| {
+                                        x.push_str(com.as_str());
+                                        x
+                                    })
+                                    .unwrap_or(String::from(com.as_str())),
+                            );
+                        }
+                        Rule::annotation => {
+                            // take care of them here
+                        }
+                        Rule::line_comment | Rule::multiline_comment => (),
+                        _ => unreachable!(),
+                    }
+                }
+            }
+            _ => unreachable!(),
         }
-    };
+    }
 
-    Ok(())
+    println!(
+        "{}ms to parse {} chars",
+        time.elapsed().as_millis(),
+        src.len()
+    );
 }
--- tests/lang_ref.rs
+++ tests/lang_ref.rs
@@ -1,2 +1,38 @@
 // in here we extract all the code present in the language reference
 // and we pass it into our parser to check if it parses the code successfully
+
+use pkl_parser::parse;
+use regex::Regex;
+use select::{document::Document, predicate::Name};
+
+#[test]
+pub fn lang_ref_code() {
+    let resp =
+        reqwest::blocking::get("https://pkl-lang.org/main/current/language-reference/index.html")
+            .unwrap();
+    assert!(resp.status().is_success());
+
+    let re = Regex::new(r"\(\d\)").unwrap();
+    let code_parts = Document::from_read(resp)
+        .unwrap()
+        .find(Name("code"))
+        .filter(|n| {
+            // removes exprs present in the doc
+            !n.text().contains("toMap")
+                && !n.text().starts_with("#")
+                && !n.text().starts_with("\"")
+                && n.attr("class")
+                    .map(|x| x.contains("language-pkl"))
+                    .unwrap_or(false)
+        })
+        .map(|x| re.replace_all(&x.text(), "").to_string())
+        .collect::<Vec<_>>();
+
+    assert!(!code_parts.is_empty());
+
+    let code = code_parts.join("\n");
+
+    let parsed = parse(&code);
+    println!("{:?}", parsed);
+    assert!(parsed.is_ok());
+}
