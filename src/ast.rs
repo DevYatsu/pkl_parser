6fb5fc8b185fb5b2ae9447e5bf2907bbd90b0c68
Yatsu <yatsu.dev@gmail.com>
2024-08-25 15:56:59 +0200

    pkl type doc comments add

--- src/ast.rs
+++ src/ast.rs
@@ -7,6 +7,28 @@ mod doc_comment;
 mod expr;
 mod stmt;
 
+/// Represents a type in the `Pkl` language.
+///
+/// # Overview
+///
+/// The `Type` struct encapsulates a type definition within the `Pkl` language.
+/// A type typically has a name, optional parameters, and optional constraints
+/// or restraints associated with it. This struct is designed to hold all
+/// relevant information needed to represent a type in the `Pkl` abstract
+/// syntax tree (AST).
+///
+/// # Fields
+///
+/// * `name: (String, Span)` - This tuple contains the name of the type as a `String`
+/// along with its `Span`, which represents the location of the type name in the source code.
+///
+/// * `params: Option<Vec<(Type, Span)>>` - An optional vector of type parameters,
+/// each represented by a tuple containing a `Type` and its associated `Span`.
+/// If the type has no parameters, this will be `None`.
+///
+/// * `restraints: Option<Box<Expr>>` - An optional expression that imposes
+/// constraints or restraints on the type. This could be used to enforce certain
+/// conditions or requirements on the type. If there are no restraints, this will be `None`.
 #[derive(Debug, Clone, PartialEq)]
 pub struct Type {
     pub name: (String, Span),
